346dd73941560c157005aed12f8a32ff595c2b8b
PocaPica <caiu_cardoso@outlook.com>
2023-10-03 15:24:35 -0300

    

---
+++ src/main/java/com/lista4/listaGit/ListaGitApplication.java
@@ -0,0 +1,13 @@
+package com.lista4.listaGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lista4/listaGit/ListaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lista4.listaGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
